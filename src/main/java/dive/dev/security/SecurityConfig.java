23d1d809aeaf155e7fe7ee56ab090cc0c3b2c0bf
dive-into-dev <dive.dev.altaf@gmail.com>
2023-09-09 17:40:07 +0200

    Remove beans

--- src/main/java/dive/dev/security/SecurityConfig.java
+++ src/main/java/dive/dev/security/SecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,24 +36,6 @@ public class SecurityConfig {
 			t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		);
 		return http.build();
-	}
-	
-	@Bean
-	public DefaultMethodSecurityExpressionHandler msecurity() {
-	  DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler = 
-	      new DefaultMethodSecurityExpressionHandler();
-	  defaultMethodSecurityExpressionHandler.setDefaultRolePrefix("");
-	  return defaultMethodSecurityExpressionHandler;
-	}
-	
-	@Bean
-	public JwtAuthenticationConverter con() {
-	  JwtAuthenticationConverter c =new JwtAuthenticationConverter();
-	  JwtGrantedAuthoritiesConverter cv = new JwtGrantedAuthoritiesConverter();
-	  cv.setAuthorityPrefix(""); // Default "SCOPE_"
-      cv.setAuthoritiesClaimName("roles"); // Default "scope" or "scp"
-	  c.setJwtGrantedAuthoritiesConverter(cv);
-	  return c;
 	}
 
 }
